feb37c4876d224c172b82933dbd7acc6ab8d8908
Vivian Lund <vivian.lund@sandersrfconsulting.com>
2020-04-22 11:13:08 -0400

    upload skeleton

---
+++ cmake-build-debug/CMakeFiles/cmake_testapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_testapp.dir/main.cpp.o"
+  "cmake_testapp"
+  "cmake_testapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_testapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
